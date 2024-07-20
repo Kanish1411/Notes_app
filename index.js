2a8a93b1c032cf536701e145a1baff034b34f596
Kanish1411 <kanishkark1411@gmail.com>
2024-07-20 09:29:09 +0530

    Changes

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const btn1=document.getElementById("btn-c")
 const notesContainer = document.querySelector('.notes')
 
 const br=document.createElement("br")
+function getitems(){
+    return JSON.parse(localStorage.getItem("notes") || "[]")
+}
 
-JSON.parse(localStorage.getItem("notes") || "[]").forEach((n)=>{
+function update(id,value){
+    const notes=getitems();
+    const note=notes.filter((n)=> n.id==id)[0]
+    note.content=value;
+    localStorage.setItem("notes",JSON.stringify(notes));
+}
+
+getitems().forEach((n)=>{
     create(n.id,n.content);
 })
 function create(id,content){
@@ -13,33 +23,26 @@ function create(id,content){
     note.className="form-control";
     note.addEventListener("dblclick",()=>{
         const war=confirm("delete this");
-        if(war){
-
-        }
     })
     note.addEventListener("input",()=>{
-        const notes=JSON.parse(localStorage.getItem("notes") || "[]")
-        const ele= notes.filter((n)=>n.id==id)[0];
-        ele.content=note.value;
-        console.log(ele);
-        localStorage.setItem("notes",JSON.stringify(notes));
+       update(id,note.value);
     })
+    note.value=content;
     notesContainer.appendChild(note);
     notesContainer.appendChild(br);
-
 }
-var a=0;
+var a=parseInt((JSON.parse(localStorage.getItem("note_id") || "0")));
 function print(){
     const notes=JSON.parse(localStorage.getItem("notes") || "[]");
     const obj={
         id:a,
         content:""
-    }
+    };
     a=a+1;
+    localStorage.setItem("note_id",JSON.stringify(a));
     create(obj.id,obj.content);
     notes.push(obj);
     localStorage.setItem("notes",JSON.stringify(notes));
-
 }
 btn1.addEventListener("click",print)
 
